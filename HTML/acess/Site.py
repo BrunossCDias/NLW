4156b2f68874bd3400a0a1fa8804e0cd0617c2f7
Bruno <brunosscd@outlook.com>
2022-09-21 09:47:42 -0300

    Movendo, e organizando, criando projeto FLASK.

---
+++ HTML/acess/Site.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+# rout
+
+# função
+@app.route("/")
+def homepage():
+    return render_template("index.html")
+
+
+@app.route
+
+if __name__ == "__main__":
+    app.run(debug=True)
